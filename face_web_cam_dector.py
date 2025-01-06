280f4f9326070cd658bb68a8f528f673eb921fa1
Rajatsurya M <rajatsm@umich.edu>
2025-01-06 02:40:07 -0500

    adding all the files related to web cam video detection

--- face_web_cam_dector.py
+++ face_web_cam_dector.py
@@ -19,7 +19,7 @@ print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
+vs = VideoStream(src=1).start()
 time.sleep(2.0)
 # loop over the frames from the video stream
 while True:
